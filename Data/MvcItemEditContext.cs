f98690754cfa0c3677d8c8f868427d0e30c701ca
unknown <basiraminapple77@gmail.com>
2024-05-10 13:06:20 +0330

    Update MvcItemEditContext.cs

--- Data/MvcItemEditContext.cs
+++ Data/MvcItemEditContext.cs
@@ -1,19 +1,22 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.EntityFrameworkCore;
-using MvcEcommerce.Models;
 
 namespace MvcEcommerce.Data
 {
     public class MvcItemEditContext : DbContext
     {
-        public MvcItemEditContext (DbContextOptions<MvcItemEditContext> options)
+        public MvcItemEditContext(DbContextOptions<MvcItemEditContext> options)
             : base(options)
         {
         }
 
-        public DbSet<MvcEcommerce.Models.Item> Item { get; set; } = default!;
+        public DbSet<MvcEcommerce.Models.Item> Item { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                optionsBuilder.UseSqlite("Data Source=MvcItemEditContext.db");
+            }
+        }
     }
 }
